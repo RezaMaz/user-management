a9cf272fbf0b49fb6fb2d815e42e920f3af32fe2
Mazloom <rmazloom88@gmail.com>
2020-08-05 13:40:43 +0430

    add instructions how to run project in README.md add test for get user in service layer by helpl of Mockito

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
-Joinfaces + Spring Boot 2 user management application
+#### **User Management application(JoinFaces + Spring Boot 2)**
 
-Instructions(How to Run):
+_Instructions(How to Run):_
 
-Build it with maven and run the jar as if it was a standard jar:
+1.Install Postgres DBMS on your system. you can use below link:
+https://www.postgresql.org/download/
+ 
+create database named `modernisc` with username `postgres` and password: `141433eli`
+
+2.Build it with maven and run the jar as if it was a standard jar:
 
 java -jar target/user-management-0.0.1-SNAPSHOT.jar
 
 It will launch a JSF and Spring powered website that you can access at http://localhost:8080/index.jsf
+
+it has capabilities to create user in individual manner and batch and you can update and delete any user by icons in
+actions column in data table.
--- src/test/java/ir/modernisc/usermanagement/TestUserService.java
+++ src/test/java/ir/modernisc/usermanagement/TestUserService.java
@@ -1,12 +1,19 @@
 package ir.modernisc.usermanagement;
 
+import ir.modernisc.usermanagement.entity.User;
 import ir.modernisc.usermanagement.repository.UserDAO;
+import ir.modernisc.usermanagement.service.dto.UserDTO;
 import ir.modernisc.usermanagement.service.service.UserService;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.modelmapper.ModelMapper;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TestUserService {
@@ -15,10 +22,35 @@ public class TestUserService {
     UserService userService;
     @Mock
     UserDAO userDAO;
+    @Mock
+    ModelMapper modelMapper;
+
+    @Before
+    public void setUp() {
+        UserDTO.Info info = new UserDTO.Info();
+        info.setId(1L);
+        info.setFirstName("Reza");
+        info.setLastName("Mazloom");
+        info.setAge(30);
+
+        when(modelMapper.map(any(), any())).thenReturn(info);
+    }
 
     @Test
     public void getAllUsersTest() {
 
+        User userOne = new User();
+        userOne.setId(1L);
+        userOne.setFirstName("Reza");
+        userOne.setLastName("Mazloom");
+        userOne.setAge(30);
+
+        when(userDAO.findById(1L)).thenReturn(java.util.Optional.of(userOne));
+
+        UserDTO.Info returnedUser = userService.get(1L);
+
+        assertEquals("Reza", returnedUser.getFirstName());
+        verify(userDAO).findById(1L);
     }
 
 }
